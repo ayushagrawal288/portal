4e635445acde9d85548c3748187e805a16191c74
ayushagrawal288 <ayushagrawal288@gmail.com>
2016-11-12 16:02:12 +0530

    databse connections

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php    
-    $host = ""; 
-    $user = ""; 
-    $pass = ""; 
-    $db = "portal"; 
+    $host = "us-cdbr-iron-east-04.cleardb.net"; 
+    $user = "b91115c64b0965"; 
+    $pass = "91290dfd"; 
+    $db = "ad_a0050aee3b0907e"; 
     $p = mysql_connect($host, $user, $pass); 
     $db_found = mysql_select_db($db, $p);
 if (!$db_found) {
